b400e1d97ac865a87419224ed013a1746781525e
finlab.company <finlab.company@gmail.com>
2020-10-22 23:47:55 +0800

    chore(finlab/git):docker env commit

--- finlab/git.py
+++ finlab/git.py
@@ -1,20 +1,24 @@
 import os
 import datetime
+from django.conf import settings
 
-# BASE_DIR = os.path.abspath(os.path.join(os.getcwd(), "../../tw_stock_class"))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def git_commit():
     now = datetime.datetime.now()
     now = str(now.strftime("%Y-%m-%d %H:%M"))
+    user_name = os.getenv("GITLAB_NAME", settings.CONFIG_DATA.get("GITLAB_NAME"))
+    user_password = os.getenv("GITLAB_PASSWORD", settings.CONFIG_DATA.get("GITLAB_PASSWORD"))
+    mail = os.getenv("GMAIL", settings.CONFIG_DATA.get("GMAIL"))
     os.chdir(BASE_DIR)
-    os.system('git pull')
-    diff_msg = os.popen('git diff data/').read()
-    if diff_msg == '':
-        return 'Finish!data was not diff'
+    os.system(f"git config --local user.email {mail}")
+    os.system(f"git config --local user.name {user_name}")
+    os.system("git remote rm origin")
+    os.system(f"git remote add origin https://{user_name}:{user_password}@gitlab.com/finlab_company_class/tw_stock.git")
+    os.system("git pull origin master")
     os.system("git add data/")
     os.system("git commit -m '%s update data'" % now)
-    os.system('git push -u origin master')
-    info = 'Finish!git push successfully'
+    os.system("git push -u origin master")
+    info = "Finish!git push successfully"
     return info
