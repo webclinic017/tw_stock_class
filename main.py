7a39264aa918f3b24700647c53a4aba1b2f8c972
finlab.company <benbilly3@gmail.com>
2020-12-21 15:40:53 +0800

    fix(main):add arg in crawler_task

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from finlab.git import git_commit
 logging.basicConfig(level=logging.INFO)
 
 
-def crawler_task():
+def crawler_task(req):
     df = TwStockClassPickler().download_all()
     git_commit()
     return df
